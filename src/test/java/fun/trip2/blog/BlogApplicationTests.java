7e725108af8fe2f1a34281ed6051f8714daa8c23
Yuenggpong <446985764@qq.com>
2020-02-06 03:56:36 +0800

    Initial commit

---
+++ src/test/java/fun/trip2/blog/BlogApplicationTests.java
@@ -0,0 +1,15 @@
+package fun.trip2.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("to be or not to be , that's a question");
+	}
+
+}
